a739e15cb22640ab94848976a9861af6c5bbf289
Brymmobaggins <ibro2tablet21@yahoo.com>
2023-06-26 21:18:50 +0100

    style messge box

--- main.js
+++ main.js
@@ -10,39 +10,22 @@ const firebaseConfig = {
     measurementId: "G-NN4SZNCJZH"
 };
 
-//  intialized datbase
-firebase.initializeApp(firebaseConfig)
-
-// reference your database
-let contactFormDB = firebase.database().ref("contactForm")
-
-
 // listen for from form submit\
 document.getElementById("contact-form").addEventListener("submit",submitForm)
 
 function submitForm(e){
     e.preventDefault();
+    showSuccess()
 
-    // get value
-    let name = getInputValue('name')
-    let email = getInputValue('email')
-    let message = getInputValue('message')
-    
-    console.log(name)
-    console.log(email)
-    console.log(message)
-    
-    // alert message
-
-    
 }
 // function to get form values
 
-function getInputValue(id){
-    return document.getElementById(id).value
-}
+// function getInputValue(id){
+//     return document.getElementById(id).value
+// }
 
-// save message to firebase
-function saveMessage(name, company, email, phone,message){
+function showSuccess(){
+    // alert('Message Sent')
     
+
 }
